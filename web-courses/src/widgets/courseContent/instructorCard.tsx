acf6ef4f6cf98a84febfaadc2c973d0dbe3f1216
Javier <javieralzuu@gmail.com>
2025-12-21 21:40:21 -0300

    deleting instructor entitie

--- web-courses/src/widgets/courseContent/instructorCard.tsx
+++ web-courses/src/widgets/courseContent/instructorCard.tsx
@@ -1,25 +1,43 @@
 'use client'
 
-import type { InstructorCardProps } from "@/entities/instructor/model/instructor"
-import { Avatar, AvatarImage, AvatarFallback } from "@/shared/ui/index" // o desde shadcn
+import { Avatar, AvatarImage, AvatarFallback } from "@/shared/ui/index"
 
-export default function InstructorCard({ name, profession, image, description }: InstructorCardProps) {
+interface InstructorCardProps {
+  name: string
+  profession: string
+  image?: string
+  description: string
+}
+
+export default function InstructorCard({
+  name,
+  profession,
+  image,
+  description,
+}: InstructorCardProps) {
   return (
     <div className="bg-white dark:bg-gray-900 rounded-lg shadow p-4 space-y-3">
       <div className="flex items-center gap-3">
-        {/* Componente Avatar */}
-        <Avatar className="w-17 h-17">
+        <Avatar className="w-16 h-16">
           <AvatarImage src={image} alt={name} />
-          <AvatarFallback>{name[0]}</AvatarFallback>
+          <AvatarFallback>
+            {name.charAt(0).toUpperCase()}
+          </AvatarFallback>
         </Avatar>
 
         <div>
-          <h3 className="text-md font-semibold text-green-600">{name}</h3>
-          <p className="text-sm text-gray-700 dark:text-gray-300 underline" >{profession}</p>
+          <h3 className="text-md font-semibold text-green-600">
+            {name}
+          </h3>
+          <p className="text-sm text-gray-700 dark:text-gray-300 underline">
+            {profession}
+          </p>
         </div>
       </div>
 
-      <p className="text-sm text-gray-600 dark:text-gray-300">{description}</p>
+      <p className="text-sm text-gray-600 dark:text-gray-300">
+        {description}
+      </p>
     </div>
   )
 }
