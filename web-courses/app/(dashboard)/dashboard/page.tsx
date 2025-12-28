63e3a3696b1918aff354ad27ea146d43761babfd
Javier <javieralzuu@gmail.com>
2025-12-28 03:57:34 -0300

    adding dynamic to dashboard

--- web-courses/app/(dashboard)/dashboard/page.tsx
+++ web-courses/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { CoursesDashboard } from "@/widgets/dashboard/CoursesDashboard"
+export const dynamic = 'force-dynamic' // ← Agregá esta línea al principio
 
 export default function DashboardPage() {
   return <CoursesDashboard />
