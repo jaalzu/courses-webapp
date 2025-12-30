2d07869e6720ebd7ce64569766873ffd0ca79fea
Javier <javieralzuu@gmail.com>
2025-12-30 03:35:46 -0300

    fix a bug

--- web-courses/src/features/favorites/model/hooks/useFavoritesCourses.ts
+++ web-courses/src/features/favorites/model/hooks/useFavoritesCourses.ts
@@ -1,11 +1,10 @@
 // 📁 features/favorites/model/hooks/useFavoriteCourses.ts
 import { useFavoriteIds } from './useFavoritesIds'
-import { localStorageFavorites } from '@/features/favorites/lib/favoriteStorage'
 import { useCourseStore } from '@/entities/course/model/useCourseStore'
 import type { Course } from '@/entities/course/types'
 
 export function useFavoriteCourses(): { favoriteCourses: Course[] } {
-  const { favorites } = useFavoriteIds(localStorageFavorites)
+  const { favorites } = useFavoriteIds()
   const courses = useCourseStore(state => state.courses)
 
   const favoriteCourses = courses.filter(course => favorites.includes(course.id))
