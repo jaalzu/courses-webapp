661a88fa71422b112d83ca90feaa82d30f19ea60
Javier <javieralzuu@gmail.com>
2026-01-09 05:21:37 -0300

    fixing next config

--- web-courses/next.config.mjs
+++ web-courses/next.config.mjs
@@ -1,14 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-}
+  // Aquí podés poner imágenes, redirecciones, etc.
+};
 
-export default nextConfig  // ← export default, NO module.exports
+export default nextConfig;
